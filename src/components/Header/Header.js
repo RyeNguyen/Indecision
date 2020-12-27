9559d97875fbef05b662aaa20693391beecf78e7
RyeNguyen <48546888+RyeNguyen@users.noreply.github.com>
2020-12-27 14:02:19 +0700

    Starting the project

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div>
+            <h1>Lát nữa ăn gì?</h1>
+            <p>Phân vân không biết ăn gì? Hãy để máy tính quyết định.</p>
+        </div>
+    )
+}
+
+export default Header;
